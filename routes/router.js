056022190fd69fd5f611c1dc1b1757c0422bd2da
Diego Silva <coxcorp@gmail.com>
2022-02-28 23:01:59 -0300

    implementa router de vendas

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const productsController = require('../controllers/listProducts');
+const salesController = require('../controllers/listSales');
 
 const productRouter = express.Router();
+const salesRouter = express.Router();
 
 // Requisito 02 - Crie endpoints para listar os produtos e as vendas
 productRouter.get('/', productsController.listAllProducts);
@@ -13,6 +15,16 @@ productRouter.put('/:id', productsController.editProductById);
 // Requisito 06 - Crie um endpoint para deletar um produto
 productRouter.delete('/:id', productsController.deleteProductById);
 
+// Requisito 02 - Crie endpoints para listar os produtos e as vendas
+salesRouter.get('/', salesController.listAllSales);
+salesRouter.get('/:id', salesController.listSaleById);
+
+// Requisito 07 - Crie um endpoint para cadastrar vendas
+salesRouter.post('/');
+// Requisito 08 - Crie um endpoint para atualizar uma venda
+salesRouter.put('/:id');
+
 module.exports = {
   productRouter,
+  salesRouter,
 };
