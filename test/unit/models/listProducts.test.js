8b704d7e0e534b46a77428e2b9de9fede49127ce
Diego Silva <coxcorp@gmail.com>
2022-03-04 00:57:48 -0300

    ajuste req01

--- test/unit/models/listProducts.test.js
+++ test/unit/models/listProducts.test.js
@@ -1,7 +1,35 @@
+const sinon = require('sinon');
 const { expect } = require('chai');
+const connection = require('../../../models/connection');
 const productsModel = require('../../../models/listProducts');
 
 describe('Listar todos os produtos', () => {
+  const products = [
+    {
+      "id": 1,
+      "name": "Martelo de Thor",
+      "quantity": 10
+    },
+    {
+      "id": 2,
+      "name": "Traje de encolhimento",
+      "quantity": 20
+    },
+    {
+      "id": 3,
+      "name": "Escudo do Capitão América",
+      "quantity": 30
+    }
+  ]
+
+  before(() => {
+    sinon.stub(connection, 'execute').resolves([products], []);
+  });
+
+  after(() => {
+    connection.execute.restore();
+  });
+
   it('O resultado deve ser um array', async () => {
     const products = await productsModel.getAllProducts();
 
@@ -17,6 +45,21 @@ describe('Listar todos os produtos', () => {
 });
 
 describe('Obter produto por id', () => {
+  const product = [
+    {
+      "id": 1,
+      "name": "Martelo de Thor",
+      "quantity": 10
+    }
+  ]
+  before(() => {
+    sinon.stub(connection, 'execute').resolves([product], []);
+  });
+
+  after(() => {
+    connection.execute.restore();
+  });
+
   it('O resultado deve ser um array', async () => {
     const product = await productsModel.getProductById(1);
 
@@ -32,6 +75,13 @@ describe('Obter produto por id', () => {
 });
 
 describe('Cadastra produtos', () => {
+    before(() => {
+      sinon.stub(connection, 'execute').resolves([[{ insertId: 4}], []]);
+    });
+
+    after(() => {
+      connection.execute.restore();
+    });
   it('O resultado deve ser um object', async () => {
     const product = await productsModel.createProduct({ "name": "produto", "quantity": 10 });
 
@@ -45,6 +95,14 @@ describe('Cadastra produtos', () => {
 });
 
 describe('Edita produtos', () => {
+  before(() => {
+    sinon.stub(connection, 'execute').resolves([[], []]);
+  });
+
+  after(() => {
+    connection.execute.restore();
+  });
+
   it('O resultado deve ser um object', async () => {
     const product = await productsModel.editProduct({ "id": 1, "name": "Martelo de Thor", "quantity": 15 });
 
@@ -56,3 +114,21 @@ describe('Edita produtos', () => {
       expect(product).to.have.keys('id', 'name', 'quantity');
   });
 });
+
+// [
+// 	{
+// 		"id": 1,
+// 		"name": "Martelo de Thor",
+// 		"quantity": 10
+// 	},
+// 	{
+// 		"id": 2,
+// 		"name": "Traje de encolhimento",
+// 		"quantity": 20
+// 	},
+// 	{
+// 		"id": 3,
+// 		"name": "Escudo do Capitão América",
+// 		"quantity": 30
+// 	}
+// ]
--- test/unit/models/listSales.test.js
+++ test/unit/models/listSales.test.js
@@ -1,7 +1,38 @@
+const sinon = require('sinon');
 const { expect } = require('chai');
+const connection = require('../../../models/connection');
 const salesModel = require('../../../models/listSales');
 
 describe('Listar todos as vendas', () => {
+  const sales = [
+    {
+      "saleId": 1,
+      "date": "2022-03-04T03:05:16.000Z",
+      "productId": 1,
+      "quantity": 5
+    },
+    {
+      "saleId": 1,
+      "date": "2022-03-04T03:05:16.000Z",
+      "productId": 2,
+      "quantity": 10
+    },
+    {
+      "saleId": 2,
+      "date": "2022-03-04T03:05:16.000Z",
+      "productId": 3,
+      "quantity": 15
+    }
+  ]
+
+  before(() => {
+    sinon.stub(connection, 'execute').resolves([sales], []);
+  });
+
+  after(() => {
+    connection.execute.restore();
+  });
+
   it('O resultado deve ser um array', async () => {
     const sales = await salesModel.getAllSales();
 
@@ -17,6 +48,23 @@ describe('Listar todos as vendas', () => {
 });
 
 describe('Obter vendas por id', () => {
+  const sales = [
+    {
+      "saleId": 1,
+      "date": "2022-03-04T03:05:16.000Z",
+      "productId": 1,
+      "quantity": 5
+    }
+  ]
+  before(() => {
+    sinon.stub(connection, 'execute').resolves([sales], []);
+  });
+
+  after(() => {
+    connection.execute.restore();
+  });
+
+
   it('O resultado deve ser um array', async () => {
     const sales = await salesModel.getSaleById(1);
 
@@ -32,6 +80,14 @@ describe('Obter vendas por id', () => {
 });
 
 describe('Cadastra vendas', () => {
+  before(() => {
+    sinon.stub(connection, 'execute').resolves([[{ insertId: 3}], []]);
+  });
+
+  after(() => {
+    connection.execute.restore();
+  });
+
   it('O resultado deve ser um object', async () => {
     const product = await salesModel.createSale([{"productId": 1, "quantity": 3 }]);
 
@@ -45,6 +101,14 @@ describe('Cadastra vendas', () => {
 });
 
 describe('Edita vendas', () => {
+  before(() => {
+    sinon.stub(connection, 'execute').resolves([[], []]);
+  });
+
+  after(() => {
+    connection.execute.restore();
+  });
+
   it('O resultado deve ser um object', async () => {
     const product = await salesModel.editSaleById({ saleId: 1, productId: 1, quantity: 10 });
 
@@ -56,3 +120,24 @@ describe('Edita vendas', () => {
       expect(product).to.have.keys('saleId', 'itemUpdated');
   });
 });
+
+[
+	{
+		"saleId": 1,
+		"date": "2022-03-04T03:05:16.000Z",
+		"productId": 1,
+		"quantity": 5
+	},
+	{
+		"saleId": 1,
+		"date": "2022-03-04T03:05:16.000Z",
+		"productId": 2,
+		"quantity": 10
+	},
+	{
+		"saleId": 2,
+		"date": "2022-03-04T03:05:16.000Z",
+		"productId": 3,
+		"quantity": 15
+	}
+]
